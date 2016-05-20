57772f5a2898b3c42d66a6c3debf5d73e08ddbe5
chuho <chuho@139.com>
2016-05-20 20:48:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php include 'top.php';?>
+<BODY onLoad="javascript:popUp()">
+<iframe width="470" scrolling="no" height="20" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=1&color=%23FFFFFF&bdc=%23&icon=3&py=guanxian&wind=1&num=2"></iframe>
+<script src="/m/uaredirect.js" type="text/javascript"></script><script type="text/javascript">uaredirect("/m");</script>
+<table class="wikitable" id="wiki">
+<tr>
+<td ><a href="http://zhuhe.tw/yun">☆朱賀雲☆</a></td>
+<td ><a href="http://ipsbc.sinaapp.com">☆辦公系統☆</a></td>
+<td ><a href="https://211.156.194.132">★郵政辦公系統★</a></td>
+<td ><a href="http://t.zhuhe.tw/?post=7">青年文明號</a></td>
+<td >
+<?php     
+date_default_timezone_set('Asia/Taipei'); //默认时区     
+echo ltrim(date("萌歷y年m月d日",strtotime("-2014 year"))."<br>","0");
+?>
+</td>
+</tr> 
+</table>
+	<tr>
+	<td valign="top" width="500">
+	<!-- MSCellType="NavBody" -->
+	<h3 align="center">常用網址 方便你我</h3>
+	<p align="center">
+	<a target="_blank" href="http://www.google.com/ncr">谷歌</a>
+	<a target="_blank" href="http://bing.com">微軟</a>
+	<a target="_blank" href="http://zhuhe.tw/host">突破封鎖</a>
+	<a target="_blank" href="http://www.d5yuansu.com">D5Life</a>
+	<a target="_blank" href="http://dapenti.com">噴嚏</a>
+	<a target="_blank" href="http://ccb.com/">建設銀行</a>
+	<a target="_blank" href="http://translate.google.com/?hl=en&tab=wT">翻譯</a></p>
+	<p align="center">
+	<a target="_blank" href="https://cmule.org">電驢</a> 
+	<a target="_blank" href="http://104.236.51.108">捷克</a> 
+	<a target="_blank" href="http://www.mediafire.com">網盤</a>
+	<a target="_blank" href="https://facebook.com">FaceBook</a>
+	<a target="_blank" href="http://tuyaya.com">圖理</a>
+	<a target="_blank" href="http://postimage.org/zhlovezl">影像</a>
+	<a target="_blank" href="http://torrentkittycn.xyz">磁力</a>
+	<a target="_blank" href="http://ifanqiang.com">集合</a></p>
+	<p align="center">
+	<a target="_blank" href="https://zh.wikipedia.org">維基</a>
+	<a target="_blank" href="http://115.com">115File</a>
+	<a target="_blank" href="https://youtube.com">YouTube</a>
+	<a target="_blank" href="http://jd.com">京東</a>
+	<a target="_blank" href="http://alipay.com">阿里</a>
+	<a target="_blank" href="http://tom365.co">TOM365</a>
+	<a target="_blank" href="http://www.ccgp.gov.cn">招標</a></p>
+	<p align="center">
+	<a target="_blank" href="http://blog.sina.com.cn/mileneiyuankanmenren">奉朱爸爸</a>
+	<a target="_blank" href="http://www.slime.com.tw">史萊姆</a>
+	<a target="_blank" href="http://www.sdgwy.org">公務員</a>
+	<a target="_blank" href="http://williamlong.info">	月光博客</a>
+	<a target="_blank" href="http://dajie.com">大街</a>
+	<a target="_blank" href="http://cntv.cn">中共臺</a></p>
+	<p align="center">
+	<a target="_blank" href="http://www.clipconverter.cc">TubeDon</a>
+	<a target="_blank" href="http://www.bttiantang.com">Movie</a>
+	<a target="_blank" href="http://kej.tw/flvretriever">Flv</a>
+	<a target="_blank" href="http://www.microsoft.com">Microsoft</a>
+	<a target="_blank" href="http://www.csdn.net">Csdn</a>
+	<a target="_blank" href="https://webim.feixin.10086.cn">FetionOS</a>
+	<a target="_blank" href="http://www.taizihong.com">Heb</a></p>
+	<p align="center">
+	<a target="_blank" href="http://cpaexam.cicpa.org.cn">註冊會計</a>
+	<a target="_blank" href="http://offcn.com">中共教育</a>
+	<a target="_blank" href="http://lixian.xunlei.com">離線</a>
+	<a target="_blank" href="http://117.56.6.1">總統府</a>
+	<a target="_blank" href="http://ctext.org">國學</a>
+   	<a target="_blank" href="http://www.jb51.net">腳本</a>
+	<a target="_blank" href="http://drvsky.com">Drv</a></p>
+	<p align="center">
+	<a target="_blank" href="http://apple.com">Apple</a>
+	<a target="_blank" href="http://answers.microsoft.com">MicAnser</a>
+	<a target="_blank" href="https://skydrive.live.com">SkyDrive</a>
+	<a target="_blank" href="http://s.chinaz.com/">WebD</a>
+	<a target="_blank" href="http://xiazaiba.com">Download</a>
+	<a target="_blank" href="http://itellyou.cn/">MSDN</a></p>
+	<p align="center">
+	<a target="_blank" href="http://w3school.com.cn">SchooL</a>
+	<a target="_blank" href="http://code.google.com/p/zhuhe">zhuhecode</a>
+	<a target="_blank" href="https://icloud.com">ICouD</a>
+	<a target="_blank" href="http://ishadowsocks.com">SocKS</a>
+	<a target="_blank" href="http://www.videotool.net">Vtool</a>
+	<a target="_blank" href="https://cloudconvert.com">Conver</a>
+	<a target="_blank" href="http://wp.it168.com">WP7</a></td>
+	<td valign="top" width="217">
+	<p align="center"></p>
+<!--zhuhe-->
+<div style="width:160px;text-align:center; height:24px">
+<div id="xcolors1_1" style="0px 0px;margin: 0px 0px;align:center;overflow:hidden;">
+<div id="xcolors1_2" style="font-size:18px;line-height:16px;font-family: arial; font-weight:bold;padding: 3px 1px;text-align:center;">
+<a href="http://www.forexq.net" alt="USD 美元 汇率查询外汇牌价" title="USD 美元 匯率查詢外匯牌價" id="forex2_links" style="18px;text-decoration:none;line-height:16px;font-family: arial;" >
+<img src="http://www.forexq.net/images/NTD.gif" border="0" style="margin:0;padding:0;border:0;" />交子 匯率</a></p></p></div>
+<script src="http://www.forexq.net/z.php?z=cn_CNYs&c=94abf0&tz=Asia/Shanghai&tf=1"></script></div>
+<!--copy end here:-->
+    	<td valign="top"  width="200">
+	<!-- MSCellType="NavBody2" -->
+</p>
+<a target="_blank" href="http:///zhuhe.tw/tool/jsq.php">金融計算</a>
+<a target="_blank" href="http:///zhuhe.tw/tool/dk.php">貸款專用</a></p>
+<a target="_blank" href="http://www.cbc.gov.tw">中央銀行</a>
+<a target="_blank" href="http://www.pbc.gov.cn">中共銀行</a></p>
+<a target="_blank" href="http://www.forexconverter.org/NTD">外匯利率</a>
+<a target="_blank" href="http://www.safe.gov.cn">大陸外匯</a></p>
+<a target="_blank" href="http://psbc.com">郵儲銀行</a>
+<a target="_blank" href="http://www.sdnxs.com">農信用社</a></p>
+<a target="_blank" href="http://www.gold24.cn">上海千石</a>
+<a target="_blank" href="http://www.shgold.com">黃金交易</a></p>
+<a href="/c" title="C語言編程">朱賀編程[經典教程]</p>
+<a href="/licai" title="朱賀的個人理財">朱賀理財[專業通道]</a></p>
+<a href="http://mail.zhuhe.tw" title="朱賀的電郵服務">朱賀郵局[個人電郵]</a></p>
+<a href="/tv" target="_blank" title="朱賀電視">朱賀電視[初步測試]</a></td>
+	<tr>
+	<td valign="top" colspan="3" >
+<?php include 'foot.php';?>
+<div class="copyright"></div>
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script src="/js/jq.snow.js"></script>
+<script>
+	$(function(){
+		$.fn.snow({ 
+			minSize: 5,		//雪花的最小尺寸
+			maxSize: 50, 	//雪花的最大尺寸
+			newOn: 1000		//雪花出现的频率 这个数值越小雪花越多
+		});
+	});
+</script>
