2bbe0e10c99d53b4ef3faa2eebb78744c12a9df0
chuho <chuho@139.com>
2016-11-01 11:13:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<title>zhuhe</title>
+<meta name="keywords" content="zhuhe,朱贺,chuho,朱賀" />
+<script src="/m/uaredirect.js" type="text/javascript"></script><script type="text/javascript">uaredirect("/m");</script>
+<META http-equiv="content-type" content="text/html; charset=utf-8"><div align="center">
+<body bgcolor="#023665" text="White" link="White" alink="White" vlink="White">
+<table border="0" cellpadding="0" cellspacing="0" width="924" height="525">
+    <!-- MSTableType="layout" -->
+	<tr>
+	<td valign="top" colspan="3" height="67">
+	<!-- MSCellType="ContentHead" -->
+<div style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+<a style="text-decoration: none" href="http://zhuhe.tw">首頁</a>
+<a style="text-decoration: none" title="租用谷歌服務器" href="http://blog.zhuhe.tw">博客</a>
+<a href="http://zhuhe.tw/video" title="新服務" style="text-decoration: none">視頻</a>
+    <a style="text-decoration: none" title="朱賀常用的軟體" href="./dll">下載</a>
+<a style="text-decoration: none" title="朱賀的文檔" href=",/doc">文檔</a>
+<a style="text-decoration: none" title="未加密的愛" href="./love">永遠</a>
+<a style="text-decoration: none" title="給我電郵" href="mailto:zhuhe@zhuhe.tw">我的郵箱</a>
+<a style="text-decoration: none" title="朱賀的自我簡介" href="./zhuhe">朱賀簡介</a>&nbsp;&nbsp;&nbsp;
+<a style="text-decoration: none" target="_blank" title="朱賀寫的代碼" href="./tool">工具</a>
+    <a style="text-decoration: none" target="_blank" title="朱賀寫的工具" href="./calendar">萬年曆</a>
+<a style="text-decoration: none" title="朱賀會幫助你" href="./help">幫助</a></div>
+<!--bing-->
+<p align="center">
+<form action="http://cn.bing.com/" target="_blank">
+<table bgcolor="#FFFFFF"><tr><td>
+<input class="ee" name="q" type="text" id="input" size="36" />
+<input type="submit" name="Submit" value="朱賀搜索" />
+</td></tr></table></form>
+<!--bing-->
+	</span></td>
+	</tr>
+	<tr>
+	<td valign="top" width="498">
+	<!-- MSCellType="NavBody" -->
+</p>
+</p>
+	<p align="center">常用網址 方便你我</font></p>
+	<p align="center">
+	<a style="text-decoration: none" target="_blank" href="http://www.google.com/ncr">谷歌</a>
+	<a style="text-decoration: none" target="_blank" href="http://bing.com">微軟</a>
+    <a style="text-decoration: none" target="_blank" href="http://zhuhe.tw/host">!真正的互聯網!</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.sina.com">新浪</a> 
+	<a style="text-decoration: none" target="_blank" href="http://ccb.com/">建設銀行</a>
+	<a style="text-decoration: none" target="_blank" href="http://translate.google.com/?hl=en&tab=wT">翻譯</a></p>
+	<p align="center">
+	<a style="text-decoration: none" target="_blank" href="https://cmule.com" color="blue">電驢</a> 
+	<a target="_blank" style="text-decoration: none" href="http://104.236.51.108">捷克</a> 
+	<a style="text-decoration: none" target="_blank" href="http://www.mediafire.com">網盤</a>
+	<a style="text-decoration: none" target="_blank" href="https://www.facebook.com">FaceBook</a>
+	<a target="_blank" style="style; text-decoration: none" href="http://www.tuyaya.com">圖理</a>
+	<a target="_blank" style="text-decoration: none" href="http://postimage.org/zhlovezl">影像</a>
+	<a target="_blank" style="style; text-decoration: none" href="http://www.qiannao.com">千腦</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.ifeng.com">鳳凰</a></p>
+	<p align="center">
+	<a style="text-decoration: none" target="_blank" href="http://zh.wikipedia.org">維基</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.115.com">115File</a>
+	<a style="text-decoration: none" target="_blank" href="https://www.youtube.com">YouTube</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.jd.com">京东</a>
+	<a style="text-decoration: none" target="_blank" href="http://alipay.com">阿里</a>
+	<a style="text-decoration: none" target="_blank" href="http://org.tom365.com">TOM365</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.ccgp.gov.cn">招標</a></p>
+	<p align="center">
+	<a target="_blank" style="text-decoration: none" href="http://blog.sina.com.cn/mileneiyuankanmenren">奉朱爸爸</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.slime.com.tw">史萊姆</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.sdgwy.org">公務員</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.williamlong.info">	月光博客</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.dajie.com">大街</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.cntv.cn">中共臺</a></p>
+	<p align="center">
+	<a style="text-decoration: none" target="_blank" href="http://www.clipconverter.cc">TubeDon</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.kuaibo.com">Kabo</a>
+	<a style="text-decoration: none" target="_blank" href="http://kej.tw/flvretriever">	Flv</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.microsoft.com">Microsoft</a>
+	<a target="_blank" style="style; text-decoration: none" href="http://www.csdn.net">Csdn</a>
+	<a target="_blank" style="text-decoration: none" href="https://webim.feixin.10086.cn">FetionOS</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.taizihong.com">Heb</a></p>
+	<p align="center">
+	<a target="_blank" style="text-decoration: none" href="http://cpaexam.cicpa.org.cn">	註冊會計</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.offcn.com">中共教育</a>
+	<a target="_blank" style="text-decoration: none" href="http://lixian.xunlei.com">離線</a>
+	<a target="_blank" style="text-decoration: none" href="http://117.56.6.1">總統府</a>
+	<a target="_blank" style="text-decoration: none" href="http://ctext.org">國學</a>
+	<a style="text-decoration: none" target="_blank" href="https://skydrive.live.com" color="#0000FF">SkyDrive</a></p>
+	<p align="center">
+	<a style="text-decoration: none" target="_blank" href="http://www.apple.com">Apple</a>
+	<a style="text-decoration: none" target="_blank" href="http://answers.microsoft.com">MicAnser</a>
+	<a style="text-decoration: none" target="_blank" href="http://www.songtaste.com">SongTaste</a>
+	<a target="_blank" style="text-decoration: none" href="http://download.comsenz.com">WebD</a>
+	<a target="_blank" style="text-decoration: none" href="http://www.xiazaiba.com">Download</a>
+	<a style="text-decoration: none" target="_blank" color=" #000000" href="http://itellyou.cn/">MSDN</a></p>
+	<p align="center">
+	<a target="_blank" style="style; text-decoration: none" href="http://www.w3school.com.cn">SchooL</a>
+	<a style="text-decoration: none" target="_blank" href="http://code.google.com/p/zhuhe">zhuhecode</a>
+	<a style="text-decoration: none" target="_blank" href="https://www.icloud.com">ICouD</a>
+	<a style="text-decoration: none" target="_blank" href="http://mail.qq.com">Tcentmail</a>
+	<a target="_blank" style="style; text-decoration: none" href="http://www.taobao.com">TaoBao</a>
+	<a target="_blank" style="text-decoration: none" href="http://wp.it168.com">WP7</a></td>
+	<td valign="top" width="217">
+	<p align="center"></p>
+<!--zhuhe-->
+<p align="center">金融時報</p>
+<div style="width:183px;text-align:center; height:24px">
+<div id="xcolors1_1" style="0px 0px;margin: 0px 0px;align:center;overflow:hidden;">
+<div id="xcolors1_2" style="font-size:12px;line-height:16px;font-family: arial; font-weight:bold;padding: 3px 1px;text-align:center;">
+<a href="http://www.forexq.net/USD" alt="USD 美元 汇率查询外汇牌价" title="USD 美元 匯率查詢外匯牌價" id="forex2_links" style="14px;text-decoration:none;line-height:16px;font-family: arial;" >
+<img src="http://www.forexq.net/images/USD.gif" border="0" style="margin:0;padding:0;border:0;" />美元 匯率</a></div>
+<script src="http://www.forexq.net/z.php?z=cn_USDs&c=94abf0&tz=GMT-8&tf=5"></script></div>
+<!--copy end here:-->
+	<td valign="top" height="399" width="209">
+	<!-- MSCellType="NavBody2" -->
+</p>
+<a target="_blank" style="text-decoration: none" href="http:///zhuhe.tw/tool/jsq.php">金融計算</a>
+<a target="_blank" style="text-decoration: none" href="http:///zhuhe.tw/tool/dk.php">貸款專用</a></p>
+<a target="_blank" style="text-decoration: none" href="http://www.cbc.gov.tw">中央銀行</a>
+<a target="_blank" style="text-decoration: none" href="http://www.pbc.gov.cn">中共銀行</a></p>
+<a target="_blank" style="text-decoration: none" href="http://www.forexconverter.org/NTD">外匯利率</a>
+<a target="_blank" style="text-decoration: none" href="http://www.safe.gov.cn">大陸外匯</a></p>
+<a target="_blank" style="text-decoration: none" href="http://www.psbc.com">郵儲銀行</a>
+<a target="_blank" style="text-decoration: none" target="_blank" href="http://www.sdnxs.com">農信用社</a></p>
+<a style="text-decoration: none" target="_blank" href="http://www.gold24.cn">上海千石</a>
+<a style="text-decoration: none" target="_blank" href="http://www.shgold.com">黃金交易</a></p>
+<a href="http://zhuhe.tw/c" title="C語言編程" style="text-decoration: none">朱賀編程[經典教程]</p>
+<a href="http://zhuhe.tw/licai" title="朱賀的個人理財" style="text-decoration: none">朱賀理財[專業通道]</a></p>
+<a href="https://qiye.mail.10086.cn/login.do" title="朱賀的電郵服務" style="text-decoration: none">朱賀郵局[個人電郵]</a></p>
+<a href="http://zhuhe.tw/tv" target="_blank" title="朱賀電視" style="text-decoration: none">朱賀電視[初步測試]</a></td>
+	</tr>
+	<tr>
+	<td valign="top" colspan="3" height="59">
+	<!-- MSCellType="ContentFoot" -->
+<p align="center">
+<a style="text-decoration: none" href="http://zhuhe.tw">Republic Of China 104</a></p>
+<p align="center">建議使用[<a target="_blank" style="text-decoration: none" href="http://www.google.com.hk/chrome?hl=zh-TW&brand=CHMI">Google</a>]瀏覽器或者[<a target="_blank" style="text-decoration: none" href="http://firefox.com.cn/download/">火狐</a>]瀏覽器效果好一些
+<div class="copyright"></div>
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script src="/js/jq.snow.js"></script>
+<script>
+	$(function(){
+		$.fn.snow({ 
+			minSize: 5,		//雪花的最小尺寸
+			maxSize: 50, 	//雪花的最大尺寸
+			newOn: 1000		//雪花出现的频率 这个数值越小雪花越多
+		});
+	});
+</script>
